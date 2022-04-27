7bff1b1ecd0a3cf1e3d55907165ac2961dfd48ca
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-04-27 10:56:12 +0300

    task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Section from 'components/Section';
 import FeedbackOptions from 'components/FeedbackOptions';
+import Statistics from './components/Statistics';
+import Notification from 'components/Notification';
 
 class App extends Component {
   state = {
@@ -9,13 +11,47 @@ class App extends Component {
     bad: 0,
   };
 
+  onLeaveFeedback = e => {
+    const key = e.target.textContent.toLowerCase();
+    console.log(key);
+    this.setState(state => ({ [key]: state[key] + 1 }));
+    console.log(this.state);
+  };
+
+  countTotalFeedback = result =>
+    Object.values(result).reduce((acc, el) => acc + el);
+
+  countPositiveFeedbackPercentage = (good, total) =>
+    Math.round((good * 100) / total);
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback(this.state);
+    const PositivePercentage = this.countPositiveFeedbackPercentage(
+      good,
+      total
+    );
+
     return (
       <>
         <Section title="Please leave feedback">
-          <FeedbackOptions options={1} onLeaveFeedback={1}></FeedbackOptions>
+          <FeedbackOptions
+            onLeaveFeedback={this.onLeaveFeedback}
+          ></FeedbackOptions>
         </Section>
-        <Section title="Statistic"></Section>
+        <Section title="Statistic">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={total === 0 ? 0 : PositivePercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
       </>
     );
   }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,18 +1,23 @@
 import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
-function FeedbackOptions({ options, onLeaveFeedback }) {
+function FeedbackOptions({ onLeaveFeedback }) {
   return (
     <div className={s.FeedbackOptions}>
-      <button className={s.button}>Good</button>
-      <button className={s.button}>Neutral</button>
-      <button className={s.button}>Bad</button>
+      <button onClick={onLeaveFeedback} className={s.button}>
+        Good
+      </button>
+      <button onClick={onLeaveFeedback} className={s.button}>
+        Neutral
+      </button>
+      <button onClick={onLeaveFeedback} className={s.button}>
+        Bad
+      </button>
     </div>
   );
 }
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.number,
   onLeaveFeedback: PropTypes.func.isRequired,
 };
 
